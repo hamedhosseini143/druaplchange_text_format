7bf108040cbd7793e78b5d9377fd3c7074cd8b27
drupaldev143 <119724661+drupaldev143@users.noreply.github.com>
2024-06-30 10:31:58 +0330

    add controller

---
+++ src/Controller/ChangeTextFormatController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\change_text_format\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Returns responses for change text format routes.
+ */
+final class ChangeTextFormatController extends ControllerBase {
+
+  /**
+   * The controller constructor.
+   */
+  public function __construct(
+    private readonly EntityTypeManagerInterface $entityTypeManager,
+  ) {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container): self {
+    return new self(
+      $container->get('entity_type.manager'),
+    );
+  }
+
+  /**
+   * Builds the response.
+   */
+  public function __invoke(): array {
+
+    $build['content'] = [
+      '#type' => 'item',
+      '#markup' => $this->t('It works!'),
+    ];
+
+    return $build;
+  }
+
+}
